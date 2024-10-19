e8f0b37a54130d079cdbe90b4dec278a212bc2ec
William <codinginthestars@gmail.com>
2024-10-19 00:22:55 -0500

    functionality working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    NavLink,
+} from 'react-router-dom';
+import Appointments from './components/Appointments';
+import Doctors from './components/Doctors';
+import Patients from './components/Patients'
 import './App.css';
 
 function App() {
+  const isLinkActive = (path) => window.location.pathname === path;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="container">
+          <h1 style={{ color: "dark blue" }}>
+            GFG- Hospital Management App
+          </h1>
+          <nav>
+            <ul>
+              <li className={isLinkActive('/appointments') ? 'active' : ''}>
+                <NavLink to="/appointments">
+                  Appointments
+                </NavLink>
+              </li>
+              <li className={isLinkActive('/doctors') ? 'active' : ''}>
+                <NavLink to="/doctors">
+                  Doctors
+                </NavLink>
+              </li>
+              <li className={isLinkActive('/patients') ? 'active' : ''}>
+                <NavLink to="/patients">
+                  Patients
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path='/appointments' element={<Appointments />} />
+            <Route path='/' element={<Appointments />} />
+            <Route path='/doctors' element={<Doctors />} />
+            <Route path='/patients' element={<Patients />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
